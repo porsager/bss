b67bdbfe49ba5a33fac4344078728044d4025b88
Stephan Hoyer <ste.hoyer@gmail.com>
2017-08-11 21:49:52 +0200

    Refactor

--- tests/index.js
+++ tests/index.js
@@ -23,22 +23,19 @@ global.window = {
 const b = require('../lib').default
 const sheet = require('../lib/sheet')
 
-function nextTick() {
-  return new Promise(resolve => setTimeout(resolve, 0))
-}
-
 o('inputs', function() {
   o(b`foo: bar;`.style).deepEquals({foo: 'bar'})
   o(b`foo bar`.style).deepEquals({foo: 'bar'})
   o(b({foo: 'bar'}).style).deepEquals({foo: 'bar'})
 })
 
-o('css class generation', async function(done) {
+o('css class generation', function(done) {
   const cls = b`foo: bar;`.class
-  await nextTick()
-  o(cls).equals(sheet.classPrefix + 1)
-  o(styleEl.textContent).equals(`.${cls}{foo:bar;}`)
-  done()
+  setTimeout(() => {
+    o(cls).equals(sheet.classPrefix + 1)
+    o(styleEl.textContent).equals(`.${cls}{foo:bar;}`)
+    done()
+  })
 })
 
 o.spec('helpers', function() {
