45297550caa0abf7c881c36e2cf9e2a572ab347e
Rasmus Porsager <rasmus@porsager.com>
2018-07-28 20:59:41 +0200

    Fix #20 using objects with $nest directly

--- tests/index.js
+++ tests/index.js
@@ -121,6 +121,11 @@ o.spec('bss', function() {
     o(b.getSheet()).equals(`.${cls} th{background:blue;}.${cls} tr{background:blue;}`)
   })
 
+  o('nest objects', function() {
+    const cls = b.$nest({ th : b('background blue') }).class
+    o(b.getSheet()).equals(`.${cls} th{background:blue;}`)
+  })
+
   o('add px', function() {
     o(b`w 1`.style).deepEquals({ width: '1px' })
     o(b('width 1').style).deepEquals({ width: '1px' })
