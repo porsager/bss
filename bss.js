a161e03f86421f47ab5407610b85f4816d2dd91e
Rasmus Porsager <rasmus@porsager.com>
2020-02-11 23:26:35 +0100

    Builds

--- bss.js
+++ bss.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.b = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
   var pseudos = [
     ':active',
@@ -98,11 +98,11 @@
     w  : 'width'
   };
 
-  var cssProperties = ['float'].concat(Object.keys(
+  const cssProperties = ['float'].concat(Object.keys(
     typeof document === 'undefined'
       ? {}
       : findWidth(document.documentElement.style)
-  ).filter(function (p) { return p.indexOf('-') === -1 && p !== 'length'; }));
+  ).filter(p => p.indexOf('-') === -1 && p !== 'length'));
 
   function findWidth(obj) {
     return obj
@@ -112,30 +112,27 @@
       : {}
   }
 
-  var isProp = /^-?-?[a-z][a-z-_0-9]*$/i;
+  const isProp = /^-?-?[a-z][a-z-_0-9]*$/i;
 
-  var memoize = function (fn, cache) {
-    if ( cache === void 0 ) cache = {};
-
-    return function (item) { return item in cache
+  const memoize = (fn, cache = {}) => item =>
+    item in cache
       ? cache[item]
-      : cache[item] = fn(item); };
-  };
+      : cache[item] = fn(item);
 
   function add(style, prop, values) {
     if (prop in style) // Recursively increase specificity
-      { add(style, '!' + prop, values); }
+      add(style, '!' + prop, values);
     else
-      { style[prop] = formatValues(prop, values); }
+      style[prop] = formatValues(prop, values);
   }
 
-  var vendorMap = Object.create(null, {});
-  var vendorValuePrefix = Object.create(null, {});
+  const vendorMap = Object.create(null, {});
+  const vendorValuePrefix = Object.create(null, {});
 
-  var vendorRegex = /^(o|O|ms|MS|Ms|moz|Moz|webkit|Webkit|WebKit)([A-Z])/;
+  const vendorRegex = /^(o|O|ms|MS|Ms|moz|Moz|webkit|Webkit|WebKit)([A-Z])/;
 
-  var appendPx = memoize(function (prop) {
-    var el = document.createElement('div');
+  const appendPx = memoize(prop => {
+    const el = document.createElement('div');
 
     try {
       el.style[prop] = '1px';
@@ -159,7 +156,7 @@
   }
 
   function assign(obj, obj2) {
-    for (var key in obj2) {
+    for (const key in obj2) {
       if (obj2.hasOwnProperty(key)) {
         obj[key] = typeof obj2[key] === 'string'
           ? obj2[key]
@@ -169,38 +166,36 @@
     return obj
   }
 
-  var hyphenSeparator = /-([a-z])/g;
+  const hyphenSeparator = /-([a-z])/g;
   function hyphenToCamelCase(hyphen) {
     return hyphen.slice(hyphen.charAt(0) === '-' ? 1 : 0).replace(hyphenSeparator, function(match) {
       return match[1].toUpperCase()
     })
   }
 
-  var camelSeparator = /(\B[A-Z])/g;
+  const camelSeparator = /(\B[A-Z])/g;
   function camelCaseToHyphen(camelCase) {
     return camelCase.replace(camelSeparator, '-$1').toLowerCase()
   }
 
-  var initialMatch = /([A-Z])/g;
+  const initialMatch = /([A-Z])/g;
   function initials(camelCase) {
     return camelCase.charAt(0) + (camelCase.match(initialMatch) || []).join('').toLowerCase()
   }
 
-  var ampersandMatch = /&/g;
-  function objectToRules(style, selector, suffix, single) {
-    if ( suffix === void 0 ) suffix = '';
+  const ampersandMatch = /&/g;
+  function objectToRules(style, selector, suffix = '', single) {
+    const base = {};
+    const extra = suffix.indexOf('&') > -1 && suffix.indexOf(',') === -1 ? '' : '&';
+    let rules = [];
 
-    var base = {};
-    var extra = suffix.indexOf('&') > -1 && suffix.indexOf(',') === -1 ? '' : '&';
-    var rules = [];
-
-    Object.keys(style).forEach(function (prop) {
+    Object.keys(style).forEach(prop => {
       if (prop.charAt(0) === '@')
-        { rules.push(prop + '{' + objectToRules(style[prop], selector, suffix, single).join('') + '}'); }
+        rules.push(prop + '{' + objectToRules(style[prop], selector, suffix, single).join('') + '}');
       else if (typeof style[prop] === 'object')
-        { rules = rules.concat(objectToRules(style[prop], selector, suffix + prop, single)); }
+        rules = rules.concat(objectToRules(style[prop], selector, suffix + prop, single));
       else
-        { base[prop] = style[prop]; }
+        base[prop] = style[prop];
     });
 
     if (Object.keys(base).length) {
@@ -213,10 +208,11 @@
     return rules
   }
 
-  var selectorSplit = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
+  const selectorSplit = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
 
   function stylesToCss(style) {
-    return Object.keys(style).reduce(function (acc, prop) { return acc + propToString(prop.charAt(0) === '!' ? prop.slice(1) : prop, style[prop]); }
+    return Object.keys(style).reduce((acc, prop) =>
+      acc + propToString(prop.charAt(0) === '!' ? prop.slice(1) : prop, style[prop])
     , '')
   }
 
@@ -234,7 +230,7 @@
 
   function formatValues(prop, value) {
     return Array.isArray(value)
-      ? value.map(function (v) { return formatValue(prop, v); }).join(' ')
+      ? value.map(v => formatValue(prop, v)).join(' ')
       : typeof value === 'string'
         ? formatValues(prop, value.split(' '))
         : formatValue(prop, value)
@@ -250,26 +246,26 @@
     return prop.charAt(0) === '-' && prop.charAt(1) === '-'
   }
 
-  var classPrefix = 'b' + ('000' + ((Math.random() * 46656) | 0).toString(36)).slice(-3) +
+  const classPrefix = 'b' + ('000' + ((Math.random() * 46656) | 0).toString(36)).slice(-3) +
                       ('000' + ((Math.random() * 46656) | 0).toString(36)).slice(-3);
 
-  var styleSheet = typeof document === 'object' && document.createElement('style');
+  const styleSheet = typeof document === 'object' && document.createElement('style');
   styleSheet && document.head && document.head.appendChild(styleSheet);
   styleSheet && (styleSheet.id = classPrefix);
 
-  var sheet = styleSheet && styleSheet.sheet;
+  const sheet = styleSheet && styleSheet.sheet;
 
-  var debug = false;
-  var classes = Object.create(null, {});
-  var rules = [];
-  var count = 0;
+  let debug = false;
+  let classes = Object.create(null, {});
+  let rules = [];
+  let count = 0;
 
   function setDebug(d) {
     debug = d;
   }
 
   function getSheet() {
-    var content = rules.join('');
+    const content = rules.join('');
     rules = [];
     classes = Object.create(null, {});
     count = 0;
@@ -284,7 +280,7 @@
     rules.push(rule);
 
     if (debug)
-      { return styleSheet.textContent = rules.join('\n') }
+      return styleSheet.textContent = rules.join('\n')
 
     try {
       sheet && sheet.insertRule(rule, arguments.length > 1
@@ -296,16 +292,16 @@
   }
 
   function createClass(style) {
-    var json = JSON.stringify(style);
+    const json = JSON.stringify(style);
 
     if (json in classes)
-      { return classes[json] }
+      return classes[json]
 
-    var className = classPrefix + (++count)
+    const className = classPrefix + (++count)
         , rules = objectToRules(style, '.' + className);
 
-    for (var i = 0; i < rules.length; i++)
-      { insert(rules[i]); }
+    for (let i = 0; i < rules.length; i++)
+      insert(rules[i]);
 
     classes[json] = className;
 
@@ -314,10 +310,10 @@
 
   /* eslint no-invalid-this: 0 */
 
-  var shorts = Object.create(null);
+  const shorts = Object.create(null);
 
   function bss(input, value) {
-    var b = chain(bss);
+    const b = chain(bss);
     input && assign(b.__style, parse.apply(null, arguments));
     return b
   }
@@ -325,7 +321,7 @@
   function setProp(prop, value) {
     Object.defineProperty(bss, prop, {
       configurable: true,
-      value: value
+      value
     });
   }
 
@@ -364,18 +360,16 @@
   setProp('classPrefix', classPrefix);
 
   function chain(instance) {
-    var newInstance = Object.create(bss, {
+    const newInstance = Object.create(bss, {
       __style: {
         value: assign({}, instance.__style)
       },
       style: {
         enumerable: true,
         get: function() {
-          var this$1 = this;
-
-          return Object.keys(this.__style).reduce(function (acc, key) {
-            if (typeof this$1.__style[key] === 'number' || typeof this$1.__style[key] === 'string')
-              { acc[key.charAt(0) === '!' ? key.slice(1) : key] = this$1.__style[key]; }
+          return Object.keys(this.__style).reduce((acc, key) => {
+            if (typeof this.__style[key] === 'number' || typeof this.__style[key] === 'string')
+              acc[key.charAt(0) === '!' ? key.slice(1) : key] = this.__style[key];
             return acc
           }, {})
         }
@@ -383,18 +377,18 @@
     });
 
     if (instance === bss)
-      { bss.__style = {}; }
+      bss.__style = {};
 
     return newInstance
   }
 
-  cssProperties.forEach(function (prop) {
-    var vendor = prop.match(vendorRegex);
+  cssProperties.forEach(prop => {
+    const vendor = prop.match(vendorRegex);
     if (vendor) {
-      var unprefixed = lowercaseFirst(prop.replace(vendorRegex, '$2'));
+      const unprefixed = lowercaseFirst(prop.replace(vendorRegex, '$2'));
       if (cssProperties.indexOf(unprefixed) === -1) {
         if (unprefixed === 'flexDirection')
-          { vendorValuePrefix.flex = '-' + vendor[1].toLowerCase() + '-flex'; }
+          vendorValuePrefix.flex = '-' + vendor[1].toLowerCase() + '-flex';
 
         vendorMap[unprefixed] = prop;
         setProp(unprefixed, setter(prop));
@@ -408,7 +402,7 @@
   });
 
   setProp('content', function Content(arg) {
-    var b = chain(this);
+    const b = chain(this);
     arg === null || arg === undefined || arg === false
       ? delete b.__style.content
       : b.__style.content = '"' + arg + '"';
@@ -425,14 +419,14 @@
   });
 
   function $media(value, style) {
-    var b = chain(this);
+    const b = chain(this);
     if (value)
-      { b.__style['@media ' + value] = parse(style); }
+      b.__style['@media ' + value] = parse(style);
 
     return b
   }
 
-  var hasUrl = /^('|"|url\('|url\(")/i;
+  const hasUrl = /^('|"|url\('|url\(")/i;
   function $import(value) {
     value && insert('@import '
       + (hasUrl.test(value) ? value : '"' + value + '"')
@@ -442,17 +436,17 @@
   }
 
   function $nest(selector, properties) {
-    var b = chain(this);
+    const b = chain(this);
     if (arguments.length === 1)
-      { Object.keys(selector).forEach(function (x) { return addNest(b.__style, x, selector[x]); }); }
+      Object.keys(selector).forEach(x => addNest(b.__style, x, selector[x]));
     else if (selector)
-      { addNest(b.__style, selector, properties); }
+      addNest(b.__style, selector, properties);
 
     return b
   }
 
   function addNest(style, selector, properties) {
-    var prop = selector.split(selectorSplit).map(function (x) {
+    const prop = selector.split(selectorSplit).map(x => {
       x = x.trim();
       return (x.charAt(0) === ':' || x.charAt(0) === '[' ? '' : ' ') + x
     }).join(',&');
@@ -462,23 +456,24 @@
       : style[prop] = parse(properties);
   }
 
-  pseudos.forEach(function (name) { return setProp('$' + hyphenToCamelCase(name.replace(/:/g, '')), function Pseudo(value, style) {
-      var b = chain(this);
+  pseudos.forEach(name =>
+    setProp('$' + hyphenToCamelCase(name.replace(/:/g, '')), function Pseudo(value, style) {
+      const b = chain(this);
       if (isTagged(value))
-        { b.__style[name] = parse.apply(null, arguments); }
+        b.__style[name] = parse.apply(null, arguments);
       else if (value || style)
-        { b.__style[name + (style ? '(' + value + ')' : '')] = parse(style || value); }
+        b.__style[name + (style ? '(' + value + ')' : '')] = parse(style || value);
       return b
-    }); }
+    })
   );
 
   function setter(prop) {
     return function CssProperty(value) {
-      var b = chain(this);
+      const b = chain(this);
       if (!value && value !== 0)
-        { delete b.__style[prop]; }
+        delete b.__style[prop];
       else if (arguments.length > 0)
-        { add(b.__style, prop, Array.prototype.slice.call(arguments)); }
+        add(b.__style, prop, Array.prototype.slice.call(arguments));
 
       return b
     }
@@ -486,20 +481,20 @@
 
   function css(selector, style) {
     if (arguments.length === 1)
-      { Object.keys(selector).forEach(function (key) { return addCss(key, selector[key]); }); }
+      Object.keys(selector).forEach(key => addCss(key, selector[key]));
     else
-      { addCss(selector, style); }
+      addCss(selector, style);
 
     return chain(this)
   }
 
   function addCss(selector, style) {
-    objectToRules(parse(style), selector, '', true).forEach(function (rule) { return insert(rule); });
+    objectToRules(parse(style), selector, '', true).forEach(rule => insert(rule));
   }
 
   function helper(name, styling) {
     if (arguments.length === 1)
-      { return Object.keys(name).forEach(function (key) { return helper(key, name[key]); }) }
+      return Object.keys(name).forEach(key => helper(key, name[key]))
 
     delete bss[name]; // Needed to avoid weird get calls in chrome
 
@@ -508,8 +503,8 @@
       Object.defineProperty(bss, name, {
         configurable: true,
         value: function Helper(input) {
-          var b = chain(this);
-          var result = isTagged(input)
+          const b = chain(this);
+          const result = isTagged(input)
             ? styling(raw(input, arguments))
             : styling.apply(null, arguments);
           assign(b.__style, result.__style);
@@ -521,7 +516,7 @@
       Object.defineProperty(bss, name, {
         configurable: true,
         get: function() {
-          var b = chain(this);
+          const b = chain(this);
           assign(b.__style, parse(styling));
           return b
         }
@@ -529,36 +524,35 @@
     }
   }
 
-  bss.helper('$animate', function (value, props) { return bss.animation(bss.$keyframes(props) + ' ' + value); }
+  bss.helper('$animate', (value, props) =>
+    bss.animation(bss.$keyframes(props) + ' ' + value)
   );
 
   function short(prop) {
-    var acronym = initials(prop)
+    const acronym = initials(prop)
         , short = popular[acronym] && popular[acronym] !== prop ? prop : acronym;
 
     shorts[short] = prop;
     return short
   }
 
-  var blockEndMatch = /;(?![^("]*[)"])|\n/;
-  var commentsMatch = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*(?![^("]*[)"])/g;
-  var propSeperator = /[ :]+/;
+  const blockEndMatch = /;(?![^("]*[)"])|\n/;
+  const commentsMatch = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*(?![^("]*[)"])/g;
+  const propSeperator = /[ :]+/;
 
-  var stringToObject = memoize(function (string) {
-    var last = ''
+  const stringToObject = memoize(string => {
+    let last = ''
       , prev;
 
-    return string.trim().replace(commentsMatch, '').split(blockEndMatch).reduce(function (acc, line) {
+    return string.trim().replace(commentsMatch, '').split(blockEndMatch).reduce((acc, line) => {
       if (!line)
-        { return acc }
+        return acc
       line = last + line.trim();
-      var ref = line.replace(propSeperator, ' ').split(' ');
-      var key = ref[0];
-      var tokens = ref.slice(1);
+      const [key, ...tokens] = line.replace(propSeperator, ' ').split(' ');
 
       last = line.charAt(line.length - 1) === ',' ? line : '';
       if (last)
-        { return acc }
+        return acc
 
       if (line.charAt(0) === ',' || !isProp.test(key)) {
         acc[prev] += ' ' + line;
@@ -566,9 +560,9 @@
       }
 
       if (!key)
-        { return acc }
+        return acc
 
-      var prop = key.charAt(0) === '-' && key.charAt(1) === '-'
+      const prop = key.charAt(0) === '-' && key.charAt(1) === '-'
         ? key
         : hyphenToCamelCase(key);
 
@@ -576,11 +570,11 @@
 
       if (key in helper) {
         typeof helper[key] === 'function'
-          ? assign(acc, helper[key].apply(helper, tokens).__style)
+          ? assign(acc, helper[key](...tokens).__style)
           : assign(acc, helper[key]);
       } else if (prop in helper) {
         typeof helper[prop] === 'function'
-          ? assign(acc, helper[prop].apply(helper, tokens).__style)
+          ? assign(acc, helper[prop](...tokens).__style)
           : assign(acc, helper[prop]);
       } else if (tokens.length > 0) {
         add(acc, prev, tokens);
@@ -590,17 +584,18 @@
     }, {})
   });
 
-  var count$1 = 0;
-  var keyframeCache = {};
+  let count$1 = 0;
+  const keyframeCache = {};
 
   function keyframes(props) {
-    var content = Object.keys(props).reduce(function (acc, key) { return acc + key + '{' + stylesToCss(parse(props[key])) + '}'; }
+    const content = Object.keys(props).reduce((acc, key) =>
+      acc + key + '{' + stylesToCss(parse(props[key])) + '}'
     , '');
 
     if (content in keyframeCache)
-      { return keyframeCache[content] }
+      return keyframeCache[content]
 
-    var name = classPrefix + count$1++;
+    const name = classPrefix + count$1++;
     keyframeCache[content] = name;
     insert('@keyframes ' + name + '{' + content + '}');
 
@@ -608,11 +603,9 @@
   }
 
   function parse(input, value) {
-    var obj;
-
     if (typeof input === 'string') {
       if (typeof value === 'string' || typeof value === 'number')
-        { return (( obj = {}, obj[input] = value, obj )) }
+        return ({ [input] : value })
 
       return stringToObject(input)
     } else if (isTagged(input)) {
@@ -627,26 +620,26 @@
   }
 
   function raw(input, args) {
-    var str = '';
-    for (var i = 0; i < input.length; i++)
-      { str += input[i] + (args[i + 1] || args[i + 1] === 0 ? args[i + 1] : ''); }
+    let str = '';
+    for (let i = 0; i < input.length; i++)
+      str += input[i] + (args[i + 1] || args[i + 1] === 0 ? args[i + 1] : '');
     return str
   }
 
   function sanitize(styles) {
-    return Object.keys(styles).reduce(function (acc, key) {
-      var value = styles[key];
+    return Object.keys(styles).reduce((acc, key) => {
+      const value = styles[key];
       key = shorts[key] || key;
 
       if (!value && value !== 0 && value !== '')
-        { return acc }
+        return acc
 
       if (key === 'content' && value.charAt(0) !== '"')
-        { acc[key] = '"' + value + '"'; }
+        acc[key] = '"' + value + '"';
       else if (typeof value === 'object')
-        { acc[key] = sanitize(value); }
+        acc[key] = sanitize(value);
       else
-        { add(acc, key, value); }
+        add(acc, key, value);
 
       return acc
     }, {})
@@ -654,5 +647,5 @@
 
   return bss;
 
-}));
+})));
 //# sourceMappingURL=bss.js.map
